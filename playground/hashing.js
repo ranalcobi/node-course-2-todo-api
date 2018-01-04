625938a4cdde927028b35d22e173b46dc74d3b56
Ran Alcobi <ran@ngworkshop.co.il>
2018-01-04 15:29:06 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
+var password = 'qwe123!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$3g5YprASrvehCgewCZGX0e8t/7YoGUivrtFwG3QdXQwhRMJgqOmTW';
+
+bcrypt.compare("password",hashedPassword, (err,res) => {
+   console.log(res); 
+});
 
 // -------------------  JWT
-var data = {
-    id:4
-}
+// var data = {
+//     id:4
+// }
 
-var token = jwt.sign(data,'qwe123')
-console.log(token)
+// var token = jwt.sign(data,'qwe123')
+// console.log(token)
 
-var decoded = jwt.verify(token,'qwe123')
-console.log(decoded)
+// var decoded = jwt.verify(token,'qwe123')
+// console.log(decoded)
 
 // -------------------  HASH
 
