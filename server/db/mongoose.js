1ea1c9484a58be32f15e8b7433a401945f8257c0
Ran Alcobi <ran@ngworkshop.co.il>
2018-01-04 11:28:30 +0200

    fix heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
- mongoose.connect('mongodb://user:user123@ds239117.mlab.com:39117/todo-api').then(() => {
- }).catch((e) =>{
-     mongoose.connect(process.env.MONGODB_URI);
- });
+//  mongoose.connect('mongodb://user:user123@ds239117.mlab.com:39117/todo-api').then(() => {
+//  }).catch((e) =>{
+//      mongoose.connect(process.env.MONGODB_URI);
+//  });
 
-//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose
